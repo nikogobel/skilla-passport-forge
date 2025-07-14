730fc3c4764ee741b985ffb9fbebe83f5ca2f2d2
nikogobel <niko@venetex.biz>
2025-07-14 10:56:34 +0200

    Ändere Standort von San Francisco, CA nach München, Deutschland

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -82,7 +82,7 @@ export default function Profile() {
                 </div>
                 <div className="flex items-center justify-center gap-2">
                   <MapPin className="h-4 w-4" />
-                  San Francisco, CA
+                  Munich, Germany
                 </div>
               </div>
 
